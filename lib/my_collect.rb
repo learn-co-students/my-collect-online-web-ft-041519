de94d21bb7556aef0e6caa4f6538dcb02cec1019
Erin Stalzer <erinstalzer@gmail.com>
2019-04-16 19:46:25 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+#array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+#def my_collect(array)
+#  my_collect(array) do |name|
+#    name.split(" ").first
+#  end
+#end
 
+def my_collect(array)
+  answer_array = []
+  i = 0
+  while  i < array.length
+      answer_array << yield(array[i])
+      i+= 1
+  end
+  answer_array
+end
