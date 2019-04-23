abf5dd6ca60a6d59e2e9b02a5f395cfe303d38ef
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-23 23:16:21 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(collection)
+  new_collection = []
+  arg = 0 
+  while arg < collection.length 
+  new_collection << yield(collection[arg])
+  arg += 1 
+end
+new_collection
+end
 
